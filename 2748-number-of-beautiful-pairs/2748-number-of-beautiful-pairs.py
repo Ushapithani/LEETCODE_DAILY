5aa303c217932421faaf79eb0410bea7dca8bdc5
USHA RANI PITHANI <pithaniusha73@gmail.com>
2026-04-07 05:12:24 +0530

    Time: 91 ms (63.39%), Space: 19.3 MB (88.98%) - LeetHub

---
+++ 2748-number-of-beautiful-pairs/2748-number-of-beautiful-pairs.py
@@ -0,0 +1,13 @@
+from math import gcd
+
+class Solution:
+    def countBeautifulPairs(self, nums: list[int]) -> int:
+        count = 0
+        n = len(nums)
+        for i in range(n):
+            first_digit = int(str(nums[i])[0])
+            for j in range(i + 1, n):
+                last_digit = nums[j] % 10
+                if gcd(first_digit, last_digit) == 1:
+                    count += 1
+        return count
