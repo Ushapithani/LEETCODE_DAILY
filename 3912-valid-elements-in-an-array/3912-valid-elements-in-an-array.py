e0e5f72755ee28859cc0dae8b31774fa9c53a717
USHA RANI PITHANI <pithaniusha73@gmail.com>
2026-04-26 21:49:57 +0530

    Time: 4 ms (44.46%), Space: 19.2 MB (98.27%) - LeetHub

---
+++ 3912-valid-elements-in-an-array/3912-valid-elements-in-an-array.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findValidElements(self, nums):
+        n = len(nums)
+        
+        right_max = [0] * n
+        right_max[n-1] = 0
+        
+        for i in range(n-2, -1, -1):
+            right_max[i] = max(right_max[i+1], nums[i+1])
+        
+        result = []
+        left_max = 0
+        
+        for i in range(n):
+            if nums[i] > left_max or nums[i] > right_max[i]:
+                result.append(nums[i])
+            
+            left_max = max(left_max, nums[i])
+        
+        return result
