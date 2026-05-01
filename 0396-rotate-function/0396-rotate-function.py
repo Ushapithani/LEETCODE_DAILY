2b05992d5c6de88267c2bdfe7f92ce0925b38f4c
USHA RANI PITHANI <pithaniusha73@gmail.com>
2026-05-01 09:29:40 +0530

    Time: 136 ms (76.83%), Space: 31.1 MB (48.17%) - LeetHub

---
+++ 0396-rotate-function/0396-rotate-function.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxRotateFunction(self, nums):
+        n = len(nums)
+        total = sum(nums)
+
+        f = sum(i * num for i, num in enumerate(nums))
+        res = f
+
+        for i in range(n - 1, 0, -1):
+            f = f + total - n * nums[i]
+            res = max(res, f)
+
+        return res
