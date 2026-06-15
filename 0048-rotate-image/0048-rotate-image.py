e2d8c446982661766f5dd07f111adec31d6858bc
USHA RANI PITHANI <pithaniusha73@gmail.com>
2026-06-15 15:05:35 +0530

    Time: 0 ms (100%), Space: 19.1 MB (93.44%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -4,5 +4,4 @@ class Solution:
         for i in range(len(matrix)):
             for j in range(i+1,len(matrix[0])):
                 matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
-
         
