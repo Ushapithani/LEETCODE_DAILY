79356d8572292230ba2cd602843eb6951612b586
USHA RANI PITHANI <pithaniusha73@gmail.com>
2026-06-29 11:53:49 +0530

    Time: 4 ms (61.06%), Space: 19.8 MB (77.94%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        ans = nums[0]
+        prefix = 1
+        suffix = 1
+        n = len(nums)
+
+        for i in range(n):
+            if prefix == 0:
+                prefix = 1
+            if suffix == 0:
+                suffix = 1
+
+            prefix *= nums[i]
+            suffix *= nums[n - 1 - i]
+
+            ans = max(ans, prefix, suffix)
+
+        return ans
