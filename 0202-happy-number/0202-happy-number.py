c52bda28813e04ec5b3e9c433a23a04c0c9bf9dc
USHA RANI PITHANI <pithaniusha73@gmail.com>
2026-07-22 05:13:54 +0530

    Time: 3 ms (34.27%), Space: 19.3 MB (61.41%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,15 +1,22 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
+
         seen = set()
-        
+
         while n != 1:
-            total = 0
-            for d in str(n):
-                total += int(d) ** 2
-            n = total
-            
+
             if n in seen:
                 return False
+
             seen.add(n)
-        
+
+            total = 0
+
+            while n > 0:
+                digit = n % 10
+                total += digit * digit
+                n //= 10
+
+            n = total
+
         return True
