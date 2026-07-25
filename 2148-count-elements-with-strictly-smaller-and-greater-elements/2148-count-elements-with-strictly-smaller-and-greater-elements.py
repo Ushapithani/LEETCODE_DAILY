48d09fc0602dd30091a226589ce3af86085bdae8
USHA RANI PITHANI <pithaniusha73@gmail.com>
2026-07-25 06:09:10 +0530

    Time: 0 ms (100%), Space: 19.4 MB (20.5%) - LeetHub

--- 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.py
+++ 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.py
@@ -3,10 +3,7 @@ class Solution:
         minimum = min(nums)
         maximum = max(nums)
 
-        count = 0
+        if minimum == maximum:
+            return 0
 
-        for num in nums:
-            if num != minimum and num != maximum:
-                count += 1
-
-        return count
+        return len(nums) - nums.count(minimum) - nums.count(maximum)
