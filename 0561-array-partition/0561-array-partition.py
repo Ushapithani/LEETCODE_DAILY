6ec0da410b0e86fe2b01722441fd39097cc0c9e4
USHA RANI PITHANI <pithaniusha73@gmail.com>
2026-07-25 10:50:17 +0530

    Time: 30 ms (28.7%), Space: 21.6 MB (23.72%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.py
@@ -0,0 +1,9 @@
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        total = 0 
+        nums.sort()
+        for i in range(0,len(nums),2):
+            total+=nums[i]
+        return total
+
+        
