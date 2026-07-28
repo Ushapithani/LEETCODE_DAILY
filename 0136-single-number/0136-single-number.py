d770d1e4f2b00b987fd2483c117f412e52743fae
USHA RANI PITHANI <pithaniusha73@gmail.com>
2026-07-28 13:37:36 +0530

    Time: 4282 ms (5.01%), Space: 21.2 MB (43.23%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -2,5 +2,5 @@ class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         for x in nums:
             if nums.count(x)==1:
-                return x
+                return x 
         
