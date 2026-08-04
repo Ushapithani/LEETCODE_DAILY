314be928cf4cd5dc9a148788441d5265842dd0cd
USHA RANI PITHANI <pithaniusha73@gmail.com>
2026-08-04 06:21:05 +0530

    Time: 0 ms (100%), Space: 19.3 MB (31.53%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def rob(self, nums):
+        if len(nums) == 1:
+            return nums[0]
+
+        return max(
+            self.houseRobber(nums[:-1]),  # Exclude last house
+            self.houseRobber(nums[1:])    # Exclude first house
+        )
+
+    def houseRobber(self, nums):
+        if len(nums) == 1:
+            return nums[0]
+
+        prev = nums[0]
+        curr = max(nums[0], nums[1])
+
+        for i in range(2, len(nums)):
+            next_money = max(curr, nums[i] + prev)
+            prev = curr
+            curr = next_money
+
+        return curr
