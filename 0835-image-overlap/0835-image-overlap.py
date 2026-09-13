0dce60d706009a819f20a49ec9d8b57a4d6e3bc3
USHA RANI PITHANI <pithaniusha73@gmail.com>
2026-09-13 18:46:02 +0530

    Time: 268 ms (44.71%), Space: 19.9 MB (28.84%) - LeetHub

---
+++ 0835-image-overlap/0835-image-overlap.py
@@ -0,0 +1,30 @@
+class Solution:
+    def largestOverlap(self, img1: List[List[int]], img2: List[List[int]]) -> int:
+        points1 = []
+        points2 = []
+
+        n = len(img1)
+
+        for i in range(n):
+            for j in range(n):
+                if img1[i][j] == 1:
+                    points1.append((i, j))
+
+                if img2[i][j] == 1:
+                    points2.append((i, j))
+
+        count = {}
+        ans = 0
+
+        for x1, y1 in points1:
+            for x2, y2 in points2:
+                dx = x2 - x1
+                dy = y2 - y1
+
+                key = (dx, dy)
+
+                count[key] = count.get(key, 0) + 1
+
+                ans = max(ans, count[key])
+
+        return ans
