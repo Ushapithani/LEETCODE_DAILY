0fe93c6e2c869d0f7a98a3ba9ed84a8705526911
USHA RANI PITHANI <pithaniusha73@gmail.com>
2026-09-17 10:44:50 +0530

    Time: 0 ms (100%), Space: 19.5 MB (72.03%) - LeetHub

---
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
@@ -0,0 +1,16 @@
+class Solution:
+    def flatten(self, root):
+        current = root
+
+        while current:
+            if current.left:
+                temp = current.left
+
+                while temp.right:
+                    temp = temp.right
+
+                temp.right = current.right
+                current.right = current.left
+                current.left = None
+
+            current = current.right
