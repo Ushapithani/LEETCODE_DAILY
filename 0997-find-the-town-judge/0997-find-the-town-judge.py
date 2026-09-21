ae0d4bf8313542115e7073f4e0ba7e798bc21b61
USHA RANI PITHANI <pithaniusha73@gmail.com>
2026-09-21 14:04:04 +0530

    Time: 3 ms (98.29%), Space: 22.8 MB (22.7%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -1,13 +1,14 @@
 class Solution:
-    def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        trust_score = [0] * (n + 1)
-        
+    def findJudge(self, n: int, trust: list[list[int]]) -> int:
+        in_degree = [0] * (n + 1)
+        out_degree = [0] * (n + 1)
+
         for a, b in trust:
-            trust_score[a] -= 1
-            trust_score[b] += 1
-        
+            out_degree[a] += 1
+            in_degree[b] += 1
+
         for person in range(1, n + 1):
-            if trust_score[person] == n - 1:
+            if in_degree[person] == n - 1 and out_degree[person] == 0:
                 return person
-        
+
         return -1
