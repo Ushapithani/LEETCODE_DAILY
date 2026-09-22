b789f1839038c6de5be664d68b62b568fdb71f67
USHA RANI PITHANI <pithaniusha73@gmail.com>
2026-09-22 13:28:12 +0530

    Time: 0 ms (100%), Space: 19.5 MB (5.94%) - LeetHub

--- 3745-maximize-expression-of-three-elements/3745-maximize-expression-of-three-elements.py
+++ 3745-maximize-expression-of-three-elements/3745-maximize-expression-of-three-elements.py
@@ -1,17 +1,5 @@
 class Solution:
-    def maximizeExpressionOfThree(self, nums: List[int]) -> int:
-        largest = float("-inf")
-        second_largest = float("-inf")
-        smallest = float("inf")
+    def maximizeExpressionOfThree(self, nums):
+        nums.sort()
 
-        for num in nums:
-            if num < smallest:
-                smallest = num
-
-            if num >= largest:
-                second_largest = largest
-                largest = num
-            elif num > second_largest:
-                second_largest = num
-
-        return largest + second_largest - smallest
+        return nums[-1] + nums[-2] - nums[0]
